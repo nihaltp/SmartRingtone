261cafaeca17ef2b36ef6b4a6fc5d73dcab4a14b
NIHAL  T P <nihaltpnki@gmail.com>
2026-03-01 15:09:37 +0530

    Move Ringtone class to its own file and update imports

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:ringtone_changer/models/ringtone.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
@@ -25,22 +26,6 @@ class MyApp extends StatelessWidget {
       ),
       home: const MyHomePage(title: 'Ringtone Rotator'),
     );
-  }
-}
-
-class Ringtone {
-  final int id;
-  final String name;
-  final String path;
-
-  Ringtone({required this.id, required this.name, required this.path});
-
-  Map<String, dynamic> toMap() {
-    return {'id': id, 'name': name, 'path': path};
-  }
-
-  factory Ringtone.fromMap(Map<String, dynamic> map) {
-    return Ringtone(id: map['id'], name: map['name'], path: map['path']);
   }
 }
 
