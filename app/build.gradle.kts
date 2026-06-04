b25a902ce9601d4507529de89456b30251ca0abd
NIHAL  T P <nihaltpnki@gmail.com>
2026-06-04 23:59:59 +0530

    chore: bump version to v1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.nihaltp.smartringtone"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
